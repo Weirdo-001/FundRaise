0551f8b1cc98cfbeb13e911ce6e33d7a78bc7f14
Aarushi Sharma <155827199+Weirdo-001@users.noreply.github.com>
2025-10-10 23:49:31 +0530

    Delete backend directory

--- backend/src/main/java/com/example/fundRaise/TransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.fundRaise;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    // You can add custom query methods here if needed
-}
